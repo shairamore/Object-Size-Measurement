10fc0229f29811c622ff5429a4837304f2389775
Runjhun Ratawal <runjhunratawal@gmail.com>
2021-11-11 00:24:37 +0530

    Prprocessing of image

--- Project2.py
+++ Project2.py
@@ -1,8 +1,18 @@
-print('Hi')
-print("Hello")
+import numpy as np
+import cv2
 
-ajdflkajdgklasj
+img_path = "example_01.jpg"
+image = cv2.imread(img_path)
+cv2.imshow('',image)
+cv2.waitKey(0) 
 
 
-print("GOT IT!!" + "I'm excited to learn git!!!")
+grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(grayscale, (9, 9), 0)
+img_edge = cv2.Canny(blur, 50, 100)
+img_edge = cv2.dilate(img_edge, None, iterations=1)
+img_edge = cv2.erode(img_edge, None, iterations=1)
+
+cv2.imshow('',img_edge)
+cv2.waitKey(0) 
 
