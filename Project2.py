80193342b8f0e0575541922ae8e99f778c0b2131
Runjhun Ratawal <runjhunratawal@gmail.com>
2021-12-02 16:35:51 +0530

    Object Detection using OpenCV

--- Project2.py
+++ Project2.py
@@ -6,11 +6,20 @@ import numpy as np
 import imutils
 import cv2
 
-#storing the image in a variable
+videoCaptureObject = cv2.VideoCapture(0)  #here 1 represents secondary camera
+result = True
+while(result):
+    ret,frame = videoCaptureObject.read()
+    cv2.imwrite("NewPicture.jpg",frame)
+    result = False
+videoCaptureObject.release()
+cv2.destroyAllWindows()
+
+#img_path = "NewPicture.jpg"
 img_path = "example_01.jpg"
 image = cv2.imread(img_path)
-cv2.imshow('Example 1 Image',image)  #syntax of imshow - imshow('name of the window',image)
-cv2.waitKey(0)   #holding the window with the image on the screen till user presses any button
+cv2.imshow('IMAGE ',image)  #syntax of imshow - imshow('name of the window',image)
+cv2.waitKey(0)
 
 
 #Preprocessing the image
@@ -20,8 +29,8 @@ img_edge = cv2.Canny(blur, 50, 100)
 img_edge = cv2.dilate(img_edge, None, iterations=1)
 img_edge = cv2.erode(img_edge, None, iterations=1)
 
-cv2.imshow('Preprocessed Image - Example 1',img_edge)
-cv2.waitKey(0) 
+#cv2.imshow('  ',img_edge)
+#cv2.waitKey(0) 
 
 
 #Object Segmentation
@@ -35,7 +44,38 @@ cnts = imutils.grab_contours(cnts)
 # Remove contours which are not large enough
 cnts = [x for x in cnts if cv2.contourArea(x) > 100]
 
-cv2.drawContours(img_edge, cnts, -1, (0,255,0), 3)
+#cv2.drawContours(img_edge, cnts, -1, (0,255,0), 3)
 
-cv2.imshow(" ", img_edge)
+#cv2.imshow(" ", img_edge)
+#cv2.waitKey(0) 
+
+#Creating reference box
+ref_object = cnts[0]
+box = cv2.minAreaRect(ref_object)
+box = cv2.boxPoints(box)
+box = np.array(box, dtype="int")
+box = perspective.order_points(box)
+(tl, tr, br, bl) = box #tl = top left ; tr= top right ; br= bottom right ; bl=bottom left
+dist_in_pixel = euclidean(tl, tr)  #finding euclidean distance between the opposite sides of the box
+dist_in_cm = 2
+pixel_per_cm = dist_in_pixel/dist_in_cm
+
+#repeating the process for every object contour in the image
+for cnt in cnts:
+	box = cv2.minAreaRect(cnt)
+	box = cv2.boxPoints(box)
+	box = np.array(box, dtype="int")
+	box = perspective.order_points(box)
+	(tl, tr, br, bl) = box
+	cv2.drawContours(image, [box.astype("int")], -1, (255, 0, 0), 2)
+	mid_pt_horizontal = (tl[0] + int(abs(tr[0] - tl[0])/2), tl[1] + int(abs(tr[1] - tl[1])/2))
+	mid_pt_verticle = (tr[0] + int(abs(tr[0] - br[0])/2), tr[1] + int(abs(tr[1] - br[1])/2))
+	wid = euclidean(tl, tr)/pixel_per_cm
+	ht = euclidean(tr, br)/pixel_per_cm
+	cv2.putText(image, "{:.1f}cm".format(wid), (int(mid_pt_horizontal[0] - 15), int(mid_pt_horizontal[1] - 10)), 
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+	cv2.putText(image, "{:.1f}cm".format(ht), (int(mid_pt_verticle[0] + 10), int(mid_pt_verticle[1])), 
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+cv2.imshow(' ',image)
 cv2.waitKey(0) 
